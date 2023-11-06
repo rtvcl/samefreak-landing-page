035c3beac544c7a351c63a40bf3c4e010af42463
rtcl <rtvcl.boss@gmail.com>
2023-11-06 22:11:21 +0700

    setup html content

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,123 @@
-import Image from 'next/image'
+import Image from "next/image";
+import BannerPNG from "@/assets/banner.png";
+import Showcase1PNG from "@/assets/showcase-1.png";
+import MusicSVG from "@/assets/music-svg";
+import BeersSVG from "@/assets/beers-svg";
+import TicketSVG from "@/assets/ticket-svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      {/* hero */}
+      <div>
+        <h1>SAMEFREAK</h1>
+        <h4>FIND YOUR SAME-FREQUENCIES SOULMATE</h4>
+        <Image src={BannerPNG} alt="banner" quality={100} />
+        <div>
+          <p>
+            No more awkward silences – dive straight into the songs and places
+            that fuel your passion, and put a whole new spin on match-making.
+          </p>
+          <form>
+            <input />
+            <button type="submit">Join the waiting list</button>
+          </form>
+        </div>
+      </div>
+      {/* highlight 1 */}
+      <div>
+        <div>
+          <h3>Music Powered</h3>
+          <h2>
+            Connect on the tunes that make you tick and find the perfect
+            first-date spots.
+          </h2>
+        </div>
+        <Image src={Showcase1PNG} alt="showcase 1" />
+      </div>
+
+      {/* highligh 2 */}
+      <div>
+        <div>
+          <h2>Unleash your inner Freak. Match based on your interests.</h2>
+          <ul>
+            <li>
+              <div>
+                <MusicSVG />
+              </div>
+              <div>
+                <h4>Musical Match</h4>
+                <p>
+                  Find your perfect duet with music taste matching! Whether
+                  it&apos;s the same bands or beats, we tune into love.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <BeersSVG />
+              </div>
+              <div>
+                <h4>Venue Vibes</h4>
+                <p>
+                  Connect in the crowd! Match with someone who shares your vibe
+                  and discover new hangout spots together.Find your perfect duet
+                  with music taste matching! Whether it&apos;s the same bands or
+                  beats, we tune into love.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <TicketSVG />
+              </div>
+              <div>
+                <h4>Festival Flings</h4>
+                <p>
+                  Swipe to match and meet at the best events – let the festival
+                  vibes set the stage for romance.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <div>
+            <h4>What makes SameFreak stand out?</h4>
+            <p>
+              Envision a dating app powered by Spotify and interests,
+              that&apos;s SameFreak! We prioritize music as the key to
+              compatibility, enhancing deeper connections through meeting
+              like-minded individuals at designated events or locales.
+            </p>
+          </div>
+          <div>
+            <h4>How does the matching process work?</h4>
+            <p>
+              Our algorithm uses genre preferences, favorite artists, and event
+              interests to match people who share similar musical tastes,
+              ensuring a harmonious and exciting first date experience.
+            </p>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* footer */}
+      <div>
+        <div>
+          <h3>Joing Now</h3>
+          <p>
+            No more awkward silences – dive straight into the songs and places
+            that fuel your passion, and put a whole new spin on match-making.
+          </p>
+          <form>
+            <input />
+            <button type="submit">Join the waiting list</button>
+          </form>
+        </div>
+        <footer>2023 © SameFreak Inc., All rights reserved.</footer>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
